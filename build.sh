c6d8bfc06215465348c95cb1152e0258d7025f0e
Oliver <5808434+oliverclozov@users.noreply.github.com>
2018-08-29 20:18:26 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,11 +13,14 @@ VCS_REF=$(git rev-parse --short HEAD)
 
 # Fetch the latest Kali debootstrap script from git
 curl "http://git.kali.org/gitweb/?p=packages/debootstrap.git;a=blob_plain;f=scripts/kali;h=50d7ef5b4e9e905cc6da8655416cdf3ef559911e;hb=refs/heads/kali/master" > kali-debootstrap &&\
-sudo debootstrap kali-rolling ./kali-root https://http.kali.org/kali ./kali-debootstrap &&\
+sudo debootstrap kali-rolling ./kali-root http://repo.kali.org/kali ./kali-debootstrap &&\
 sudo tar -C kali-root -c . | sudo docker import - kalilinux/kali-linux-docker &&\
+sudo rm -rf ./kali-root &&\
+TAG=$(sudo docker run -it kalilinux/kali-linux-docker awk '{print $NF}' /etc/debian_version | sed 's/\r$//' ) &&\
+echo "Tagging kali with $TAG" &&\
 sudo docker tag kalilinux/kali-linux-docker:$VERSION kalilinux/kali-linux-docker:$TAG &&\
 echo "Labeling kali" &&\
-sudo docker build --squash --rm -t kalilinux/kali-linux-docker:$VERSION \
+sudo docker build --squash -it kalilinux/kali-linux-docker:$VERSION /bin/bash \
 --build-arg BUILD_DATE=$BUILD_DATE \
 --build-arg VERSION=$VERSION \
 --build-arg VCS_URL=$VCS_URL \
